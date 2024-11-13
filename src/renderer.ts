541ad3527734f9ce4cfb74d98ac2923a2bd01d5a
Roberth González <reliutg@gmail.com>
2024-11-13 21:27:28 +0100

    Update Monaco editor version to 0.52.0 for improved features and performance

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,13 +2,14 @@ import { createApp } from 'vue'
 import { install as VueMonacoEditorPlugin } from '@guolao/vue-monaco-editor'
 
 import './style.css'
+
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(VueMonacoEditorPlugin, {
   paths: {
-    vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.43.0/min/vs'
+    vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.52.0/min/vs',
   },
 })
 
