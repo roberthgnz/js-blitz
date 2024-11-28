b2860f5a272a6acd25128ecab9824f477ebdd45a
Roberth González <reliutg@gmail.com>
2024-11-28 18:15:36 +0100

    feat: integrate floating-vue for tooltip functionality in the application

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
 import { install as VueMonacoEditorPlugin } from '@guolao/vue-monaco-editor'
+import { Tooltip, vTooltip } from 'floating-vue'
 
 import './style.css'
+import 'floating-vue/dist/style.css'
 
 import App from './App.vue'
 
 const app = createApp(App)
+
+app.directive('tooltip', vTooltip)
+app.component('VTooltip', Tooltip)
 
 app.use(VueMonacoEditorPlugin, {
   paths: {
