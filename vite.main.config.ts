d009aaf40e98330a43840bcb85b0784048276aa1
Roberth González <reliutg@gmail.com>
2024-11-30 15:36:14 +0100

    feat: add path aliases for improved module resolution in TypeScript and Vite config

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import path from 'path'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config
 export default defineConfig({
-  plugins: [vue()]
-});
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@/electron': path.resolve(__dirname, './electron'),
+      '@/src': path.resolve(__dirname, './src'),
+      '@/utils': path.resolve(__dirname, './utils'),
+    },
+  },
+})
